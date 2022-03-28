4346d5de63d651795477099d10f26003db465990
119140183-AuliaThomiFikriansyah <91131489+119140183-AuliaThomiFikriansyah@users.noreply.github.com>
2022-03-28 11:37:34 +0700

    Add files via upload

---
+++ database/data.js
@@ -0,0 +1,50 @@
+export const Pelabuhan =[
+    {
+        plbh_nama: 'Bakauheni',key:1,
+        plbh_kota: 'Lampung',
+        kuota: 500
+    },
+    {
+        plbh_nama : 'Merak',key:2,
+        plbh_kota : 'Banten',
+        kuota: 500
+    },
+    {
+        plbh_nama : 'Ketapang',key:3,
+        plbh_kota : 'Banyuwangi',
+        kuota: 500
+    },
+    {
+        plbh_nama : 'Tanjung Mas',key:4,
+        plbh_kota : 'Semarang',
+        kuota: 500
+    },
+    {
+        plbh_nama : 'Dwikora Harbour',key:5,
+        plbh_kota : 'Pontianak',
+        kuota: 500
+    },
+    
+]
+export  const Pemesan=[
+    {
+        Nama_pemesan : '',key:1,
+        Umur :'',
+        Identitas :'',
+        Kode_pemesan:''
+    }
+]
+
+export const Pesanan =[
+    {
+        Status : '',key:1,
+        Asal: '',
+        Tujuan: '',
+        Layanan:'',
+        Tgl :'',
+        Waktu:'',
+        Jumlah:'',
+        Total:'',
+        Kode_pemesan:'',
+    }
+]
